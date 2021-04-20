76be00815efb6b4f91c044d2c01f73c35a9cbfba
GimYoungPhil <youngphil.gim@gmail.com>
2021-04-20 14:59:20 +0900

    add ch09

---
+++ src/ch09/ex0911_1.py
@@ -0,0 +1,26 @@
+import winsound
+
+print("사운드 시작")
+winsound.Beep(261, 1000)
+winsound.Beep(293, 1000)
+winsound.Beep(329, 1000)
+winsound.Beep(349, 1000)
+winsound.Beep(392, 1000)
+insound.Beep(440, 1000)w
+winsound.Beep(493, 1000)
+winsound.Beep(523, 1000)
+print("사운드 종료")
+
+# winsound.Beep(261, 200)
+# winsound.Beep(329, 200)
+# winsound.Beep(392, 200)
+
+# winsound.Beep(261, 200)
+# winsound.Beep(329, 200)
+# winsound.Beep(392, 200)
+
+# winsound.Beep(440, 200)
+# winsound.Beep(440, 200)
+# winsound.Beep(440, 200)
+
+# winsound.Beep(392, 2000)
