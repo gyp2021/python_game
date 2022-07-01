859a837eb88e7718f6b9bc022d636d1a194cbcc9
YoungPhilGim <youngphil.gim@kasset.co.kr>
2022-07-01 15:13:22 +0900

    fixed

---
+++ src/ch02/test4.py
@@ -0,0 +1,7 @@
+print("range testing...")
+
+for y in range(8):
+    print(y)
+
+for x in range(8, -1, -1):
+    print(x)
---
+++ src/ch10/pygame_system.py
@@ -0,0 +1,9 @@
+import pygame
+import sys
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+def main():
+  pygame.init()
+  pygame.display.set_caption("First Pygame")
